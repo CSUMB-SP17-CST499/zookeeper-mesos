77a1bab365c0dde366eed61d8d7256f48e5682fa
Doug Alexander <doalexander@csumb.edu>
2017-03-08 14:30:47 -0800

    Skeleton for class

---
+++ src/ZKScheduler.cpp
@@ -0,0 +1,26 @@
+
+#include <fstream>
+#include <iostream>
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <list>
+#include <map>
+
+//Our protobuffs
+//#include "curated.pb.h"
+
+//#include mesos::Resources;
+
+//using namespace mesos;
+
+class ZKScheduler : public Scheduler
+{
+	
+};
+
+int main(int argc, char** argv)
+{
+	
+}
