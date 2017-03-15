c24cf91c9d9cbf6c418f29c04c8c84ebe5b22662
Doug Alexander <doalexander@csumb.edu>
2017-03-15 02:57:33 -0700

    Reading tasks from file

--- docs/prototypes/algorithm/algo_proto.py
+++ docs/prototypes/algorithm/algo_proto.py
@@ -1,5 +1,14 @@
 from task import *
 
+f_reader = open('sample_tasks.txt','r')
+f_tasks = [list(line.rstrip().split(',')) for line in f_reader.readlines()]
+f_reader.close()
+f_reader = open('sample_offers.txt','r')
+f_offers = [line.rstrip() for line in f_reader.readlines()]
+f_reader.close()
+
+print(f_tasks)
+print(f_offers)
 a = Task(2,3,1,0)
 b = Task(3,3,1,1)
 c = Task(5,3,1,2)
@@ -7,7 +16,7 @@ d = Task(2,3,1,3)
 e = Task(4,3,1,4)
 f = Task(6,3,1,5)
 
-tasks = [a,b,c,d,e,f]
+tasks = [Task(args[0],args[1],args[2],args[3],)  for args in f_tasks]
 tasks.sort(key=lambda t: t.cpu,reverse=True)
 for index,task in enumerate(tasks):
 	print("Task: "+str(index))
--- docs/prototypes/algorithm/task.py
+++ docs/prototypes/algorithm/task.py
@@ -9,4 +9,11 @@ class Task:
 		
 	def __str__(self):
 		return "{\n\t"+"'cpu': "+str(self.cpu)+",\n\t"+"'memory': "+str(self.mem)+",\n\t"+"'priority': "+str(self.prio)+",\n\t"+"'elevation_time': "+str(self.elev)+"\n}"
-	
+
+class Offer:
+	def __init__(self, cpu, mem):
+		self.cpu = cpu
+		self.mem = mem
+		
+	def __str__(self):
+		return "{\n\t"+"'cpu': "+str(self.cpu)+",\n\t"+"'memory': "+str(self.mem)+"\n}"
