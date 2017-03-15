f6773965da3498cbeae7dc15fb688d0504a1377b
Doug Alexander <doalexander@csumb.edu>
2017-03-15 04:05:03 -0700

    Working on test.py algorithm behavior'

--- docs/prototypes/algorithm/test.py
+++ docs/prototypes/algorithm/test.py
@@ -1,12 +1,18 @@
-offers = [2,3,5,2,4,6]
+offers = [2,3,5,2,4,6,7]
 offers.sort(reverse=True)
 tasks = [5,6]
 tasks.sort(reverse=True)
-offer_matrix = [(x,y,0 if x < y else 1, w,z) for z,x in enumerate(offers) for w,y in enumerate(tasks)]
-offer_permutations = [ ( ( x,y ) if ( x[2] == 1 and y[2] == 1 ) and ( x[3] != y[3] ) and ( x[4] != y[4] )  else 123  ) for x in offer_matrix for y in offer_matrix]
-offer_permutations = list(filter(lambda a: a != 123, offer_permutations))
+offer_matrix = [(1 if y >=x else 0) for z,x in enumerate(tasks) for w,y in enumerate(offers)]
+#offer_matrix = [(x,y,0 if x < y else 1, w,z) for z,x in enumerate(offers) for w,y in enumerate(tasks)]
+#offer_permutations = [ ( ( x,y ) if ( x[2] == 1 and y[2] == 1 ) and ( x[3] != y[3] ) and ( x[4] != y[4] )  else 123  ) for x in offer_matrix for y in offer_matrix]
+#offer_permutations = list(filter(lambda a: a != 123, offer_permutations))
 #offer_binary = [ (x,y,0 if 
 print(offers)
 print(tasks)
 print(offer_matrix)
-print(offer_permutations)
+
+for x in range(0,len(offers)):
+	for y in range(x,len(offers)-1):
+		if( offer_matrix[x]==1 and offer_matrix[len(offers)+1+y]==1):
+			print(offers[x],offers[y+1])
+#print(offer_permutations)
