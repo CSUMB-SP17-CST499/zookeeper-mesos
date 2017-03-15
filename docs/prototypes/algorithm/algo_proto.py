2db59948a76bf6436debd93047b86b5f0af0ede2
Doug Alexander <doalexander@csumb.edu>
2017-03-15 03:06:43 -0700

    Tasks and Offers read in. Comprehensions working to turn into class. Improve Class Object parameter passing in comprehension

--- docs/prototypes/algorithm/algo_proto.py
+++ docs/prototypes/algorithm/algo_proto.py
@@ -1,24 +1,40 @@
+# Task and Offers class from task.py
 from task import *
 
+# Read in tasks and turn into list of lists
+# Comprehension splits line removes comma and converts to int
 f_reader = open('sample_tasks.txt','r')
-f_tasks = [list(line.rstrip().split(',')) for line in f_reader.readlines()]
+f_tasks = [list(map(int, line.rstrip().split(','))) for line in f_reader.readlines()]
 f_reader.close()
+
+# Same as above but with less no priority of elevation_time
 f_reader = open('sample_offers.txt','r')
-f_offers = [line.rstrip() for line in f_reader.readlines()]
+f_offers = [list(map(int, line.rstrip().split(','))) for line in f_reader.readlines()]
 f_reader.close()
 
-print(f_tasks)
-print(f_offers)
-a = Task(2,3,1,0)
-b = Task(3,3,1,1)
-c = Task(5,3,1,2)
-d = Task(2,3,1,3)
-e = Task(4,3,1,4)
-f = Task(6,3,1,5)
+# Uncomment to see Tasks and Offers as processed files
+# print(f_tasks)
+# print(f_offers)
 
-tasks = [Task(args[0],args[1],args[2],args[3],)  for args in f_tasks]
+# Comprehension to create class objects
+# Would be nice to assign parameters better
+tasks = [Task(args[0],args[1],args[2],args[3])  for args in f_tasks]
 tasks.sort(key=lambda t: t.cpu,reverse=True)
+
+# Same as above with less parameters
+offers = [Offer(args[0],args[1])  for args in f_offers]
+offers.sort(key=lambda t: t.cpu,reverse=True)
+
+
+# Uncomment to see Tasks as json
 for index,task in enumerate(tasks):
 	print("Task: "+str(index))
 	print(task)
-print(a.cpu)
+
+#Delineation between Tasks and Offers
+print("\n-----------------------------------\n")
+
+# Uncomment to see Offers as json
+for index,offers in enumerate(offers):
+	print("Offer: "+str(index))
+	print(offers)
